4dde293eaab6043eb8953a72d8e5e6e70b235dac
Aubrey Masten and Sean Pierce <sean@>
2017-07-18 16:02:55 -0700

    increase sequence length to 256

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -1,9 +1,9 @@
 class Sequence < ApplicationRecord
   validates :title, :data, :width, :speed, presence: true
   validates :speed, :width, numericality: true
-  validates :data, length: { is: 64 }
+  validates :data, length: { is: 256 }
   validates :data, format: {
-    with: /[01]{64}/,
+    with: /[01]{256}/,
     message: "can only contain 0s or 1s"
   }
 end
