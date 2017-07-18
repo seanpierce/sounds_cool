bb275dfc3a66156c5d54b1b0146a549474e73486
seanpierce <contact@seanpierce.us>
2017-07-17 19:25:18 -0700

    freq mod

--- synth/scripts.js
+++ synth/scripts.js
@@ -2,15 +2,24 @@ window.onload = function() {
   var context = new window.AudioContext(),
       osc = context.createOscillator(),
       vol = context.createGain(),
+      freqGain = context.createGain(),
+      lfo = context.createOscillator(),
+      lfoRate = document.getElementById('lfo-rate'),
       volControl = document.getElementById('volume'),
       pitchControl = document.getElementById('pitch');
 
   vol.gain.value = volControl.value;
   vol.connect(context.destination);
 
-  osc.frequency.value = 80;
+  osc.frequency.value = 440;
   osc.type = "square";
   osc.connect(vol);
+
+  freqGain.gain.value = 100;
+  freqGain.connect(osc.frequency);
+
+  lfo.frequency.value = 1;
+  lfo.connect(freqGain);
 
   volControl.addEventListener('input', function() {
     vol.gain.value = this.value;
@@ -22,10 +31,17 @@ window.onload = function() {
     $('#current-pitch').text(this.value);
   });
 
+  lfoRate.addEventListener('input', function() {
+    lfo.frequency.value = this.value;
+    $('#current-lfo-rate').text(this.value);
+  });
+
   $('#synth-on').click(function() {
     osc.start();
+    lfo.start();
   });
   $('#synth-off').click(function() {
     osc.stop();
+    lfo.stop();
   });
 };
