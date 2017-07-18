2d843069b61f745d30900b65d2d62809bd57bc2b
Aubrey Masten and Sean Pierce <sean@>
2017-07-18 10:11:22 -0700

    add ui for synth element testing

--- synth/scripts.js
+++ synth/scripts.js
@@ -7,13 +7,37 @@ window.onload = function() {
       panner = context.createStereoPanner(),
       freqGain = context.createGain(),
       lfo = context.createOscillator(),
+      filter = context.createBiquadFilter(),
+      filterCutoffControl = document.getElementById('filter'),
+      repeaterSpeed = document.getElementById('speed'),
+      filterResoControl = document.getElementById('reso'),
       panControl = document.getElementById('panner'),
       lfoRate = document.getElementById('lfo-rate'),
       vcaControl = document.getElementById('volume'),
       pitchControl = document.getElementById('pitch');
 
+
+      // repeater function
+      var intervalId;
+      function repeater(time) {
+        var intervalId = setInterval(function(){
+          vca.gain.value = 1;
+          setTimeout(function() {
+            vca.gain.value = 0;
+          }, (time / 2));
+        }, time);
+        return intervalId;
+      }
+
+
+  // lowpass filter
+  filter.type = "lowpass";
+  filter.frequency.value = 10000;
+  filter.Q.value = 5;
+  filter.connect(context.destination);
+
   // panner node
-  panner.connect(context.destination);
+  panner.connect(filter);
 
   // volume node
   vca.gain.value = vcaControl.value;
@@ -23,6 +47,10 @@ window.onload = function() {
   vco.frequency.value = 80;
   vco.type = "square";
   vco.connect(vca);
+
+  // start oscillators
+  vco.start();
+  lfo.start();
 
   // lfo node
   freqGain.gain.value = 100;
@@ -48,16 +76,25 @@ window.onload = function() {
 
   panControl.addEventListener('input', function() {
     panner.pan.value = this.value;
-    $('#current-pan-position').text(this.value);
+    $('#current-pan-left').text((this.value * -1)*10);
+    $('#current-pan-right').text((this.value)*10);
+  });
+
+  filterCutoffControl.addEventListener('input', function() {
+    filter.frequency.value = this.value;
+    $('#current-filter-cutoff').text(this.value);
+  });
+
+  filterResoControl.addEventListener('input', function() {
+    filter.Q.value = this.value;
+    $('#current-filter-reso').text(this.value);
   });
 
   // turn vco on, off
   $('#synth-on').click(function() {
-    vco.start();
-    lfo.start();
+    intervalId = repeater(repeaterSpeed.value);
   });
   $('#synth-off').click(function() {
-    vco.stop();
-    lfo.stop();
+    clearInterval(intervalId);
   });
 };
