166b667f6828a3a6c298d12e3847093d47bb5672
Aubrey Masten <mastenaub@gmail.com>
2017-07-18 09:56:52 -0700

    validate presence of sequence properties

---
+++ spec/models/sequence_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Sequence, type: :model do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :data }
+  it { should validate_presence_of :width }
+  it { should validate_presence_of :speed }
+end
