d70b624e155c9a4dd2fa97b0113121dd59e78710
Aubrey Masten <mastenaub@gmail.com>
2017-07-18 10:16:57 -0700

    add format validation for data

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -2,4 +2,8 @@ class Sequence < ApplicationRecord
   validates :title, :data, :width, :speed, presence: true
   validates :speed, :width, numericality: true
   validates :data, length: { is: 64 }
+  validates :data, format: {
+    with: /[01]{64}/,
+    message: "data can contain 0s or 1s"
+  }
 end
--- spec/models/sequence_spec.rb
+++ spec/models/sequence_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Sequence, type: :model do
   it { should validate_numericality_of :speed }
   it { should validate_numericality_of :width }
   it { should validate_length_of(:data).is_equal_to 64}
+  it do
+    should allow_values('0000000000000000000000000000000000000000000000000000000000000000', '1111111111111111111111111111111111111111111111111111111111111111')
+    .for(:data)
+  end
+  it do
+    should_not  allow_values('2000000000000000000000000000000000000000000000000000000000000000', '2111111111111111111111111111111111111111111111111111111111111111')
+    .for(:data)
+  end
 end
