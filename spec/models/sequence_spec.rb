4af3db3afee3287371b4131c94c4942e8d9af710
Aubrey Masten <mastenaub@gmail.com>
2017-07-18 09:59:20 -0700

    add numericality validation of sequence width and speed

--- spec/models/sequence_spec.rb
+++ spec/models/sequence_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Sequence, type: :model do
   it { should validate_presence_of :data }
   it { should validate_presence_of :width }
   it { should validate_presence_of :speed }
+  it { should validate_numericality_of :speed }
+  it { should validate_numericality_of :width }
 end
