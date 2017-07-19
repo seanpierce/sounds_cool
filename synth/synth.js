a96791015d4dae32da25f8b4ca0410de64856bba
Sean Pierce <sumler.sean@gmail.com>
2017-07-19 11:54:53 -0700

    add visualizer

--- synth/synth.js
+++ synth/synth.js
@@ -31,8 +31,7 @@ class Oscillator {
     this.lfo.start();
   }
 
-  trigger(){
-    // this.vca.gain.value = 1;
+  trigger() {
     this.vca.gain.setTargetAtTime(1, this.context.currentTime, 0.01);
     this.vca.gain.setTargetAtTime(0, this.context.currentTime + 0.01, release);
   }
@@ -55,6 +54,8 @@ class Sequencer {
     this.genVCOs();
     this.masterVolume.gain.value = 0.5;
     this.masterVolume.connect(this.filter);
+    this.analyser = this.context.createAnalyser();
+    this.masterVolume.connect(this.analyser);
     this.filter.connect(this.context.destination);
   }
   genVCOs() {
@@ -129,6 +130,55 @@ class Sequencer {
 
 var sequencer = new Sequencer(16, 'c_major',"0000000000000001000000000000000000000000000010000000000000010000000000000010000000000000010000000000000010000000000000000000000100000010010000000000010000000000000010000000000000010000000000000000000000000000000010000000000010000000000000000000001000000001" );
 
+// ------------- BEGIN ANALYSER BLOCK
+// var analyser = sequencer.context.createAnalyser();
+// analyser.fftSize = 2048;
+// var bufferLength = analyser.frequencyBinCount;
+// var dataArray = new Uint8Array(bufferLength);
+// analyser.getByteTimeDomainData(dataArray);
+//
+// // Get a canvas defined with ID "oscilloscope"
+// // var canvas = document.getElementById("oscilloscope");
+// // var canvasContext = canvas.getContext("2d");
+//
+// // draw an oscilloscope of the current audio source
+// function draw(canvas, canvasContext) {
+//
+//   drawVisual = requestAnimationFrame(draw);
+//
+//   analyser.getByteTimeDomainData(dataArray);
+//
+//   canvasContext.fillStyle = 'rgba(255, 255, 255, 0)';
+//   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+//
+//   canvasContext.lineWidth = 2;
+//   canvasContext.strokeStyle = 'rgb(0, 0, 0)';
+//
+//   canvasContext.beginPath();
+//
+//   var sliceWidth = canvas.width * 1.0 / bufferLength;
+//   var x = 0;
+//
+//   for (var i = 0; i < bufferLength; i++) {
+//
+//     var v = dataArray[i] / 128.0;
+//     var y = v * canvas.height / 2;
+//
+//     if (i === 0) {
+//       canvasContext.moveTo(x, y);
+//     } else {
+//       canvasContext.lineTo(x, y);
+//     }
+//
+//     x += sliceWidth;
+//   }
+//
+//   canvasContext.lineTo(canvas.width, canvas.height / 2);
+//   canvasContext.stroke();
+// };
+// ----------- END ANALYZER BLOCK
+
+
 function toggleMarked(element){
 
   if(element.hasClass("marked")){
@@ -194,6 +244,85 @@ function getPattern(id){
 // ----------------------------------------- UI
 $(document).ready(function(){
 
+
+  // var canvas = document.getElementById("oscilloscope");
+  // var canvasContext = canvas.getContext("2d");
+  // console.log(canvas);
+  // console.log(canvasContext);
+  // draw(canvas, canvasContext);
+
+  var canvasContext = document.getElementById('oscilloscope').getContext('2d');
+
+  function draw() {
+    drawScope(sequencer.analyser, canvasContext);
+
+    requestAnimationFrame(draw);
+  }
+
+  // function drawSpectrum(analyser, ctx) {
+  //   var width = ctx.canvas.width;
+  //   var height = ctx.canvas.height;
+  //   var freqData = new Uint8Array(analyser.frequencyBinCount);
+  //   var scaling = height / 256;
+  //
+  //   analyser.getByteFrequencyData(freqData);
+  //
+  //   ctx.fillStyle = 'rgba(0, 0, 0, 0)';
+  //   ctx.fillRect(0, 0, width, height);
+  //
+  //   ctx.lineWidth = 2;
+  //   ctx.strokeStyle = 'rgb(0, 0, 0)';
+  //   ctx.beginPath();
+  //
+  //   for (var x = 0; x < width; x++)
+  //     ctx.lineTo(x, height - freqData[x] * scaling);
+  //
+  //   ctx.stroke();
+  // }
+
+  function drawScope(analyser, ctx) {
+    var width = ctx.canvas.width;
+    var height = ctx.canvas.height;
+    var timeData = new Uint8Array(analyser.frequencyBinCount);
+    var scaling = height / 256;
+    var risingEdge = 0;
+    var edgeThreshold = 5;
+
+    analyser.getByteTimeDomainData(timeData);
+
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'rgb(0, 0, 0)';
+    ctx.beginPath();
+
+    // No buffer overrun protection
+    while (timeData[risingEdge++] - 128 > 0 && risingEdge <= width);
+    if (risingEdge >= width) risingEdge = 0;
+
+    while (timeData[risingEdge++] - 128 < edgeThreshold && risingEdge <= width);
+    if (risingEdge >= width) risingEdge = 0;
+
+    for (var x = risingEdge; x < timeData.length && x - risingEdge < width; x++)
+      ctx.lineTo(x - risingEdge, height - timeData[x] * scaling);
+
+    ctx.stroke();
+  }
+
+
+  draw();
+
+
+
+
+
+
+
+
+
+
+
   // request all sequences form API
   $.get("http://localhost:3000/sequences").done(sequences => {
     sequences.forEach(sequence => {
@@ -204,7 +333,6 @@ $(document).ready(function(){
     });
     $('.pattern-button').click(function() {
       sequencer.sequenceData = $(this).attr('data-pattern');
-      console.log('dang');
     });
     $('.pattern-button').click(function(){
       var id = $(this).attr('data-id');
