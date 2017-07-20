5a3a65a8b06f97a9a664d7f6b091ce66ae119471
Aubrey Masten and Sean Pierce <aubrey@>
2017-07-20 11:07:04 -0700

    comment and reformat code

--- synth/synth.js
+++ synth/synth.js
@@ -48,6 +48,7 @@ class Sequencer {
     this.attack = 0.01;
     this.release = 0.15;
     this.speed = 200;
+    this.key = 'c_major_garbage';
 
     // creation
     this.masterVolume = this.context.createGain();
@@ -73,22 +74,26 @@ class Sequencer {
 
     // initialization
     this.lfo.start();
-    this.genVCOs();
-
+    this.generateOscillators();
   }
-  
-  genVCOs() {
-    for(var i=1; i <= this.vcoCount; i++) {
-      var pitch = KEYS['c_major_garbage'][i-1];
-      var osc = new Oscillator(pitch, this.context, this.masterVolume, this);
+
+  generateOscillators() {
+    for(let i = 0; i < this.vcoCount; i++) {
+      let pitch = KEYS[this.key][i];
+      let osc = new Oscillator(pitch, this.context, this.masterVolume, this);
       this.oscillators.push(osc);
     }
   }
 
-  toggle(id){
-    var str = this.sequenceData.split("")
+  toggle(id) {
+    let str = this.sequenceData.split("")
     str[id] = str[id] === "1" ? "0" : "1"
     this.sequenceData = str.join("")
+  }
+
+  // TODO: call method on save click
+  // updates sequence in API
+  updateSequence() {
     $.ajax({
       url: `http://localhost:3000/sequences/${this.sequenceId}`,
       type: "PUT",
@@ -98,30 +103,25 @@ class Sequencer {
         speed: 200,
         width: 300
       }
-    }).done(function(data){
-    })
+    });
   }
 
   // plays oscillators at indeces in osillatorArray
   // corresponding to indeces in current step sequence data where digit is 1
   step() {
-    var stepDads = this.sequenceData.substr(this.currentStep * this.vcoCount, this.vcoCount)
-    var indeces = this.getIndeces(stepDads)
-    indeces.forEach(index => {
+    let stepDads = this.sequenceData.substr(this.currentStep * this.vcoCount, this.vcoCount);
+    this.getIndeces(stepDads).forEach(index => {
       this.oscillators[index].trigger();
     });
 
-    highlightRow(this.currentStep, indeces)
+    highlightRow(this.currentStep)
 
-    if(this.currentStep < 15) {
-      this.currentStep += 1;
-    } else {
-      this.currentStep = 0;
-    }
-    this.timeoutId = setTimeout(function(scope){ scope.step()}, this.speed, this)
+    this.currentStep = this.currentStep < 15 ? this.currentStep + 1 : 0;
+
+    this.timeoutId = setTimeout(function(scope){ scope.step()}, this.speed, this);
   }
 
-  getIndeces(stepDads){
+  getIndeces(stepDads) {
     let indeces = [];
     for(let i = 0; i < stepDads.length; i++){
       if(stepDads[i] === "1"){ indeces.push(i)}
@@ -130,38 +130,29 @@ class Sequencer {
   }
 
   startSequencer() {
-    // starts interval
-    // saves intervalID as instance variable
-    // interval triggers step method, every speed milliseconds
-    this.timeoutId = setTimeout(function(scope){ scope.step()}, this.speed, this)
+    this.timeoutId = setTimeout(function(scope){ scope.step() }, this.speed, this);
   }
 
   stopSequencer() {
     clearTimeout(this.timeoutId);
   }
-
 }
 
-var sequencer = new Sequencer(16, 'c_major',"0000000000000001000000000000000000000000000010000000000000010000000000000010000000000000010000000000000010000000000000000000000100000010010000000000010000000000000010000000000000010000000000000000000000000000000010000000000010000000000000000000001000000001" );
+// TODO: initialize sequencer with AJAX request
+const SEQUENCER = new Sequencer(16, 'c_major',"0000000000000001000000000000000000000000000010000000000000010000000000000010000000000000010000000000000010000000000000000000000100000010010000000000010000000000000010000000000000010000000000000000000000000000000010000000000010000000000000000000001000000001" );
 
 function toggleMarked(element){
-
-  if(element.hasClass("marked")){
-    element.removeClass("marked")
-  } else {
-    element.addClass("marked")
-  }
+  element.hasClass("marked") ? element.removeClass("marked") : element.addClass("marked");
 }
 
 function generateGrid(){
-  for(var i = 0; i < sequencer.vcoCount; i++){
+  for(let i = 0; i < SEQUENCER.vcoCount; i++){
     $("#grid").append(`<div class="col" id="col-${i}"></div>`)
-    for(var n = 0; n < sequencer.vcoCount; n++){
-      $(`#col-${i}`).append(`<div class="square" id="square-${(i*16)+n}"></div>`)
-
+    for(let n = 0; n < SEQUENCER.vcoCount; n++){
+      $(`#col-${i}`).append(`<div class="square" id="square-${(i*16)+n}"></div>`);
       $(`#square-${(i*16)+n}`).click(function(){
-        var id = $(this).attr("id").match(/[0-9]+/)[0];
-        sequencer.toggle(id);
+        let id = $(this).attr("id").match(/[0-9]+/)[0];
+        SEQUENCER.toggle(id);
         toggleMarked($(this));
       })
     }
@@ -170,9 +161,9 @@ function generateGrid(){
 }
 
 function placeMarkers(){
-  for(var i = 0; i < 16; i++){
-    var rowData = sequencer.sequenceData.substr(i*16, 16);
-    for(var n = 0; n < 16; n++){
+  for(let i = 0; i < 16; i++){
+    let rowData = SEQUENCER.sequenceData.substr(i*16, 16);
+    for(let n = 0; n < 16; n++){
       $(`#col-${i} #square-${(i*16)+n}`).removeClass("marked")
       if(rowData.charAt(n) === "1"){
         $(`#col-${i} #square-${(i*16)+n}`).addClass("marked")
@@ -181,37 +172,29 @@ function placeMarkers(){
   }
 }
 
-function highlightRow(colId, indeces){
-  var prev;
-  if(colId === 0){
-    prev = 15;
-  } else {
-    prev = colId - 1;
-  }
-  for(var i = 0; i < 16; i++){
+// TODO: resolve sequence highlight animation
+function highlightRow(colId){
+  let prev = colId === 0 ? 15 : colId - 1;
+
+  for(let i = 0; i < 16; i++){
     $(`#col-${prev}`).removeClass("active")
     $(`#col-${colId}`).addClass("active")
-    // $(`#col-${prev} #square-${(prev*16)+i}`).removeClass("active pressed");
-    // $(`#col-${colId} #square-${(colId*16)+i}`).addClass("active");
   }
-  // indeces.forEach(index => {
-  //   $(`#col-${colId} #square-${(colId*16)+index}`).addClass("pressed")
-  // });
 }
 
 function getPattern(id){
   $.get(`http://localhost:3000/sequences/${id}`).done(sequence => {
-    sequencer.sequenceTitle = sequence.title;
-    sequencer.sequenceData = sequence.data;
-    sequencer.sequenceId = sequence.id;
+    SEQUENCER.sequenceTitle = sequence.title;
+    SEQUENCER.sequenceData = sequence.data;
+    SEQUENCER.sequenceId = sequence.id;
     placeMarkers();
   });
 }
 
 function placePreviewMarkers(sequence){
-  for(var i = 0; i < 16; i++){
-    var rowData = sequence.data.substr(i*16, 16);
-    for(var n = 0; n < 16; n++){
+  for(let i = 0; i < 16; i++){
+    let rowData = sequence.data.substr(i*16, 16);
+    for(let n = 0; n < 16; n++){
       $(`#preview-${sequence.id} #p-col-${i} #p-square-${(i*16)+n}`).removeClass("p-marked")
       if(rowData.charAt(n) === "1"){
         $(`#preview-${sequence.id} #p-col-${i} #p-square-${(i*16)+n}`).addClass("p-marked")
@@ -223,37 +206,32 @@ function placePreviewMarkers(sequence){
 function generatePreview(sequence){
   $('#grids').append(`<div class="preview" id="preview-${sequence.id}"></div>`)
   $(`#preview-${sequence.id}`).css("background-color",`rgb(${Math.floor(Math.random()*(255+1))},${Math.floor(Math.random()*(255+1))},${Math.floor(Math.random()*(255+1))})`)
-  for(var i = 0; i < sequencer.vcoCount; i++){
+  for(let i = 0; i < SEQUENCER.vcoCount; i++){
     $(`#preview-${sequence.id}`).append(`<div class="p-col" id="p-col-${i}"></div>`)
-    for(var n = 0; n < sequencer.vcoCount; n++){
+    for(let n = 0; n < SEQUENCER.vcoCount; n++){
       $(`#preview-${sequence.id} #p-col-${i}`).append(`<div class="p-square" id="p-square-${(i*16)+n}"></div>`)
     }
   }
   $(`#preview-${sequence.id}`).click(function(){
-    // console.log('GIMME DATA');
-    // var id = $(this).attr('data-id');
     getPattern(sequence.id);
-  })
+  });
   placePreviewMarkers(sequence);
 }
 
 // ----------------------------------------- UI
 $(document).ready(function(){
-
-  // var canvasContext = document.getElementById('oscilloscope').getContext('2d');
-
   function draw() {
-    drawScope(sequencer.analyser, canvasContext);
+    drawScope(SEQUENCER.analyser, canvasContext);
     requestAnimationFrame(draw);
   }
 
   function drawScope(analyser, ctx) {
-    var width = ctx.canvas.width;
-    var height = ctx.canvas.height;
-    var timeData = new Uint8Array(analyser.frequencyBinCount);
-    var scaling = height / 256;
-    var risingEdge = 0;
-    var edgeThreshold = 0;
+    let width = ctx.canvas.width,
+        height = ctx.canvas.height,
+        timeData = new Uint8Array(analyser.frequencyBinCount),
+        scaling = height / 256,
+        risingEdge = 0,
+        edgeThreshold = 0;
 
     analyser.getByteTimeDomainData(timeData);
 
@@ -271,7 +249,7 @@ $(document).ready(function(){
     while (timeData[risingEdge++] - 128 < edgeThreshold && risingEdge <= width);
     if (risingEdge >= width) risingEdge = 0;
 
-    for (var x = risingEdge; x < timeData.length && x - risingEdge < width; x++)
+    for (let x = risingEdge; x < timeData.length && x - risingEdge < width; x++)
       ctx.lineTo(x - risingEdge, height - timeData[x] * scaling);
 
     ctx.stroke();
@@ -289,67 +267,67 @@ $(document).ready(function(){
     });
     $('.pattern-button').click(function(){
       console.log('dang');
-      var id = $(this).attr('data-id');
+      let id = $(this).attr('data-id');
       getPattern(id);
     });
   });
 
   generateGrid();
-  // sequencer.oscillators.forEach((osc, index) => {
+  // SEQUENCER.oscillators.forEach((osc, index) => {
   //     $(".triggers").append(`<button class="button" id="${index}">${index+1}</button>`);
   // });
 
   $(".button").click(function(){
-    var index = parseInt($(this).attr('id'), 10);
-    sequencer.oscillators[index].trigger();
+    let index = parseInt($(this).attr('id'), 10);
+    SEQUENCER.oscillators[index].trigger();
 
   });
 
   // master volume
   document.getElementById('master-volume').addEventListener('input', function() {
-    sequencer.masterVolume.gain.value = this.value;
+    SEQUENCER.masterVolume.gain.value = this.value;
     $('#current-master-volume').text(this.value);
   });
 
   // filter
   document.getElementById('filter').addEventListener('input', function() {
-    sequencer.filter.frequency.value = this.value;
+    SEQUENCER.filter.frequency.value = this.value;
     $('#current-filter-cutoff').text(this.value);
   });
 
   // filter resonance
   document.getElementById('reso').addEventListener('input', function() {
-    sequencer.filter.Q.value = this.value;
+    SEQUENCER.filter.Q.value = this.value;
     $('#current-filter-reso').text(this.value);
   });
 
   // filter type
   document.getElementById('filter-type').addEventListener('change', function() {
-    sequencer.filter.type = $("input[name=filter]:checked").val();
+    SEQUENCER.filter.type = $("input[name=filter]:checked").val();
   });
 
   // sequence speed
   document.getElementById('speed').addEventListener('input', function() {
-    sequencer.speed = this.value;
+    SEQUENCER.speed = this.value;
     $('#current-speed').text(this.value);
   });
 
   // attack
   // NOTE: why why why does to precision get funky?
   document.getElementById('attack').addEventListener('input', function() {
-    sequencer.attack = Number.parseFloat(this.value)
+    SEQUENCER.attack = Number.parseFloat(this.value)
     $('#current-attack').text(this.value);
   });
 
   // release
   document.getElementById('release').addEventListener('input', function() {
-    sequencer.release = this.value;
+    SEQUENCER.release = this.value;
     $('#current-release').text(this.value);
   });
 
   // lfo-vco-gain
   document.getElementById('lfo-gain').addEventListener('input', function() {
-    sequencer.oscillators.forEach(oscillator => {
+    SEQUENCER.oscillators.forEach(oscillator => {
       oscillator.lfoGain.gain.value = this.value;
     });
     $('#current-lfo-gain').text(this.value);
@@ -357,7 +335,7 @@ $(document).ready(function(){
 
   // lfo-vco-frequency
   document.getElementById('lfo-frequency').addEventListener('input', function() {
-    sequencer.oscillators.forEach(oscillator => {
+    SEQUENCER.oscillators.forEach(oscillator => {
       oscillator.lfo.frequency.value = this.value;
     });
     $('#current-lfo-frequency').text(this.value);
@@ -365,34 +343,34 @@ $(document).ready(function(){
 
   // lfo-filter-gain
   document.getElementById('lfo-filter-gain').addEventListener('input', function() {
-    sequencer.lfoGain.gain.value = this.value;
+    SEQUENCER.lfoGain.gain.value = this.value;
     $('#current-lfo-filter-gain').text(this.value);
   });
 
   // lfo-filter-frequency
   document.getElementById('lfo-filter-frequency').addEventListener('input', function() {
-    sequencer.lfo.frequency.value= this.value;
+    SEQUENCER.lfo.frequency.value= this.value;
     $('#current-lfo-filter-frequency').text(this.value);
   });
 
   // vco coarse tune
   document.getElementById('tune-up').addEventListener('click', function() {
     $('#current-tuning').text(parseInt($('#current-tuning').text()) + 1);
-    sequencer.oscillators.forEach(oscillator => {
+    SEQUENCER.oscillators.forEach(oscillator => {
       oscillator.vco.frequency.value += 100;
     });
     $('#current-lfo-frequency').text(this.value);
   });
   document.getElementById('tune-down').addEventListener('click', function() {
     $('#current-tuning').text(parseInt($('#current-tuning').text()) - 1);
-    sequencer.oscillators.forEach(oscillator => {
+    SEQUENCER.oscillators.forEach(oscillator => {
       oscillator.vco.frequency.value -= 100;
     });
   });
 
   // vco fine tune
   document.getElementById('fine-tune').addEventListener('input', function() {
-    sequencer.oscillators.forEach(oscillator => {
+    SEQUENCER.oscillators.forEach(oscillator => {
       oscillator.vco.detune.value = parseInt(this.value  * 10);
     });
     $('#current-fine-tune').text(this.value);
@@ -400,16 +378,16 @@ $(document).ready(function(){
 
   // vco waveshape
   document.getElementById('vco-waveshape').addEventListener('change', function() {
-    sequencer.oscillators.forEach(oscillator => {
+    SEQUENCER.oscillators.forEach(oscillator => {
       oscillator.vco.type = $("input[name=waveshape]:checked").val();
     });
   });
 
   $('#start-sequencer').click(function(){
-    sequencer.startSequencer()
+    SEQUENCER.startSequencer()
   });
 
   $('#stop-sequencer').click(function(){
-    sequencer.stopSequencer()
+    SEQUENCER.stopSequencer()
   });
 });
